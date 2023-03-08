17f69ab1e78c44b47568f7fb911c9dac86771020
PavanManiKanta48 <pavanmanikanta484@gmail.com>
2023-03-08 11:58:26 +0530

    Count down app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Display from "./Display";
 
 function App() {
+  const endTime = new Date("july 11, 2023 00:00:00").getTime();
+  const [currentTime, setcurrentTime] = useState(new Date().getTime());
+  const gap = endTime - currentTime;
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  const remainigDays = Math.floor(gap / days);
+  const remainigHours = Math.floor((gap % days) / hours);
+  const remainigMinutes = Math.floor((gap % hours) / minutes);
+  const remainigSeconds = Math.floor((gap % minutes) / seconds);
+
+  useEffect(() => {
+    setTimeout(() => setcurrentTime(new Date().getTime()), 1000);
+  }, [currentTime]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <Display
+          days={remainigDays}
+          hours={remainigHours}
+          minutes={remainigMinutes}
+          seconds={remainigSeconds}
+        />
+      </center>
     </div>
   );
 }
